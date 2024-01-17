ae1f7e83f59956d39021cbfc5a90225b675d13a1
garrettsiegel <garrett@submurgedcreative.com>
2024-01-17 00:18:30 -0500

    Basic Sanity frontend config

--- astro/astro.config.mjs
+++ astro/astro.config.mjs
@@ -1,4 +1,14 @@
 import { defineConfig } from 'astro/config';
 
+import sanity from "astro-sanity";
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [
+    sanity({
+    projectId: 'phdgiyik',
+    dataset: 'production',
+    apiVersion: '2021-03-25',
+    useCdn: true,
+  })],
+});
